024ce71342f6e309bf4139df889ddfae071f8684
Tobias Klumpp <tobias.klumpp@toklumpp.net>
2025-03-11 19:01:06 +0000

    Fix resume button

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,8 +50,6 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 }
 
 // add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
 
 
 
@@ -61,7 +59,6 @@ const selectItems = document.querySelectorAll("[data-select-item]");
 const selectValue = document.querySelector("[data-selecct-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
-select.addEventListener("click", function () { elementToggleFunc(this); });
 
 // add event in all select items
 for (let i = 0; i < selectItems.length; i++) {
